4bc765f8a0086f2826b0f2614398403be9805cd6
Binh Tran <thanhbinh.tran93@gmail.com>
2019-01-31 21:49:40 +0200

    add code splitting

--- webpack.config.js
+++ webpack.config.js
@@ -5,10 +5,25 @@ const port = process.env.PORT || 3000;
 
 module.exports = {
   mode: "development",
-  entry: "./src/index.js",
+  entry: {
+    vendor: ["semantic-ui-react"],
+    app: "./src/index.js"
+  },
   output: {
-    filename: "bundle.[hash].js",
+    filename: "[name].[hash].js",
     publicPath: "/"
+  },
+  optimization: {
+    splitChunks: {
+      cacheGroups: {
+        vendor: {
+          chunks: "initial",
+          test: "vendor",
+          name: "vendor",
+          enforce: true
+        }
+      }
+    }
   },
   devtool: "inline-source-map",
   module: {
