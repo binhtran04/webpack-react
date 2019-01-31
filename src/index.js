1608b214f6f37c7ba34d954946bcb0b71b04da9b
Binh Tran <thanhbinh.tran93@gmail.com>
2019-01-31 21:20:14 +0200

    add Hot Module Replacement

--- src/index.js
+++ src/index.js
@@ -3,3 +3,6 @@ import ReactDOM from "react-dom";
 import App from "./components/App";
 
 ReactDOM.render(<App />, document.getElementById("root"));
+
+// Webpack Hot Module Replacement API
+if (module.hot) module.hot.accept("./components/App", () => render(App));
